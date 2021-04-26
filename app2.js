e767f46f632e051e63ccb620da9bfbe066929934
pawelfiderek <pawel.fiderek@ttpsc.pl>
2021-04-26 08:17:28 +0000

    new app part added

---
+++ app2.js
@@ -0,0 +1,4 @@
+console.log('ustawiam parametry...')
+console.log('przetwarzam dane...')
+console.log("wykonuje mega skomplikowane obliczenia...")
+console.log("ostateczny wynik dzialania programu to: 7!")
